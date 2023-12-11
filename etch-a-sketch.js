e10a60119470bb7fdc40e79eee5fcea108717833
Eric Mogendi <mogendieric19@gmail.com>
2023-12-11 13:55:05 +0300

    Added js code for making 16x16 divs

---
+++ etch-a-sketch.js
@@ -0,0 +1,29 @@
+//Set important constants and variables
+const container = document.getElementById("container");
+let rows = document.getElementsByClassName("gridRow");
+let cells = document.getElementsByClassName("cell");
+
+//Creates a default grid sized 16x16
+function defaultGrid() {
+    makeRows(16);
+    makeColumns(16);
+}
+
+//Takes (rows, columns) input and makes a grid
+function makeRows (rowNum) {
+    //create rows
+    for (r=0; r<rowNum; r++) {
+        let row = document.createElement("div");
+        container.appendChild(row).className = "gridRow";
+    };
+};
+
+//Create columns
+function makeColumns(cellNum) {
+    for (i=0; i<rows.length; i++) {
+        for (let j=0; j<cellNum; j++) {
+            let newCell = document.createElement("div");
+            rows[j].appendChild(newCell).className = "cell";
+        };
+    };
+}
