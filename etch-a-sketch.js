3b132b5b36f84c718278aea9cf02cf2aa4bf31d9
Eric Mogendi <mogendieric19@gmail.com>
2023-12-11 14:02:17 +0300

    changed js code

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,29 +1,32 @@
-//Set important constants and variables
+// Sets important constants and variables
+
 const container = document.getElementById("container");
 let rows = document.getElementsByClassName("gridRow");
 let cells = document.getElementsByClassName("cell");
 
-//Creates a default grid sized 16x16
+// Creates a default grid sized 16x16
 function defaultGrid() {
     makeRows(16);
     makeColumns(16);
 }
 
-//Takes (rows, columns) input and makes a grid
-function makeRows (rowNum) {
-    //create rows
-    for (r=0; r<rowNum; r++) {
+// Takes (rows, columns) input and makes a grid
+function makeRows(rowNum) {
+
+    // Creates rows
+    for (r = 0; r < rowNum; r++) {
         let row = document.createElement("div");
         container.appendChild(row).className = "gridRow";
     };
 };
 
-//Create columns
+// Creates columns
 function makeColumns(cellNum) {
-    for (i=0; i<rows.length; i++) {
-        for (let j=0; j<cellNum; j++) {
+    for (i = 0; i < rows.length; i++) {
+        for (j = 0; j < cellNum; j++) {
             let newCell = document.createElement("div");
             rows[j].appendChild(newCell).className = "cell";
         };
+
     };
-}
+};
